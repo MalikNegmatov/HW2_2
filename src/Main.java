f80c3205378de2ede6c2d73a8a5c6ca18cf39e4f
MalikNegmatov <maliknegmatov@gmail.com>
2023-06-12 00:39:50 +0300

    Аккуратно слизали всё, как было на лайв-встрече 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 
 public class Main {
+
     public static void main(String[] args) {
+
+        Hogwarts harry = new Gryffindor("Harry Potter", 100, 100 );
+        Hogwarts draco = new Slyzerin("Draco Malfoy", 85, 85, 90, 70, 90, 80, 100);
+        Hogwarts gregoryGoyle = new Slyzerin("Gregory Goyle", 50, 50, 50, 80, 50, 50, 50);
+        Hogwarts hermione = new Gryffindor("Hermione Granger", 80, 88);
+        Hogwarts parvati = new Gryffindor("Parvati Patil", 77, 33);
+
+        gregoryGoyle.compare(draco);
+        harry.compare(draco);
+        parvati.compare(hermione);
+
+        hermione.printCompareOfStudents(draco, harry); // нужно ли и можно ли скрыть метод класса от таких вызовов?
     }
 }
